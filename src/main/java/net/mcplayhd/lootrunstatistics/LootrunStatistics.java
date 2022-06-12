40ae69a1848320dd2e9600fda99186877fa15b9f
McPlayHD <McPlayHD@gmx.ch>
2022-06-12 11:03:13 +0200

    preparing for v0.5

--- src/main/java/net/mcplayhd/lootrunstatistics/LootrunStatistics.java
+++ src/main/java/net/mcplayhd/lootrunstatistics/LootrunStatistics.java
@@ -27,7 +27,7 @@ import java.util.UUID;
 public class LootrunStatistics {
     public static final String MODID = "lootrunstatistics";
     public static final String NAME = "Lootrun Statistics";
-    public static final String VERSION = "0.4";
+    public static final String VERSION = "0.5";
 
     private static Logger logger;
 
