115efa1f2ac4fbfbdafcdd721832c7e42eea422e
McPlayHD <McPlayHD@gmx.ch>
2022-06-14 17:20:19 +0200

    notifying the player in chat when a new version is available and creating `/lootrunstatistics update` command to automatically open the latest version URL and also open the mod folder automatically. The player will be notified the first time his level is bigger than 0

--- src/main/java/net/mcplayhd/lootrunstatistics/commands/MainCommand.java
+++ src/main/java/net/mcplayhd/lootrunstatistics/commands/MainCommand.java
@@ -3,13 +3,18 @@ package net.mcplayhd.lootrunstatistics.commands;
 import com.google.common.collect.ImmutableList;
 import net.mcplayhd.lootrunstatistics.gui.CustomGui;
 import net.mcplayhd.lootrunstatistics.gui.GuiFactory;
+import net.mcplayhd.lootrunstatistics.helpers.DesktopHelper;
+import net.mcplayhd.lootrunstatistics.helpers.VersionHelper;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.client.IClientCommand;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 import java.util.List;
+
+import static net.mcplayhd.lootrunstatistics.LootrunStatistics.getLogger;
 
 public class MainCommand extends CommandBase implements IClientCommand {
 
@@ -43,6 +48,17 @@ public class MainCommand extends CommandBase implements IClientCommand {
 
     @Override
     public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args) {
+        if (args.length > 0 && args[0].equalsIgnoreCase("update")) {
+            try {
+                String version = VersionHelper.getOnlineVersion();
+                String url = "https://github.com/McPlayHD/LootrunStatistics/releases/tag/" + version;
+                DesktopHelper.openURL(url);
+                DesktopHelper.openFile(new File("mods"));
+            } catch (Exception ex) {
+                getLogger().error("Couldn't fetch current version");
+            }
+            return;
+        }
         // I can't open the gui immediately because the chat close event will also update the currentScreen.
         // `null` because I don't want the chat box to open again when closing the inventory.
         CustomGui.shouldBeDrawn = GuiFactory.getMainGui(null);
--- src/main/java/net/mcplayhd/lootrunstatistics/listeners/RenderListener.java
+++ src/main/java/net/mcplayhd/lootrunstatistics/listeners/RenderListener.java
@@ -2,16 +2,22 @@ package net.mcplayhd.lootrunstatistics.listeners;
 
 import net.mcplayhd.lootrunstatistics.LootrunStatistics;
 import net.mcplayhd.lootrunstatistics.gui.CustomGui;
+import net.mcplayhd.lootrunstatistics.helpers.VersionHelper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-import static net.mcplayhd.lootrunstatistics.LootrunStatistics.getChests;
+import static net.mcplayhd.lootrunstatistics.LootrunStatistics.*;
 
 public class RenderListener {
 
     private int lastPlayerLevel = 0;
+    private boolean seenPlayerOverLevel0 = false;
 
     // idea to only open the gui one tick later from https://github.com/albarv340/chestcountmod
     @SubscribeEvent
@@ -25,6 +31,24 @@ public class RenderListener {
     @SubscribeEvent
     public void renderWorld(RenderWorldLastEvent event) {
         int playerLevel = LootrunStatistics.getPlayerLevel();
+        if (!seenPlayerOverLevel0 && playerLevel > 0) {
+            seenPlayerOverLevel0 = true;
+            try {
+                String newVersion = VersionHelper.getNewVersion();
+                if (newVersion != null) {
+                    String link = "https://github.com/McPlayHD/LootrunStatistics/releases/tag/" + newVersion;
+                    ITextComponent text = new TextComponentString("§7\n")
+                            .appendText("§eVersion §3" + newVersion + " §eis available for §b" + LootrunStatistics.NAME + "§e.\n")
+                            .appendText("§7Click ")
+                            .appendSibling(new TextComponentString("§9[§bhere§9]").setStyle(new Style().setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lootrunstatistics update"))))
+                            .appendText(" §7to open the download page and mod folder automatically or click the following url if you prefer to do it on your own: ")
+                            .appendSibling(new TextComponentString("§e§o" + link).setStyle(new Style().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link))));
+                    getPlayer().sendMessage(text);
+                }
+            } catch (Exception ex) {
+                getLogger().error("Couldn't check for new version", ex);
+            }
+        }
         if (lastPlayerLevel != playerLevel) {
             lastPlayerLevel = playerLevel;
             getChests().updateAllNotes();
