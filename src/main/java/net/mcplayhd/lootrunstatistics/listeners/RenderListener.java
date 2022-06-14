1f606b38642fe4ad905a0e306779286d184e973b
McPlayHD <McPlayHD@gmx.ch>
2022-06-14 17:17:26 +0200

    if the player's level changes then the unlevelled chests also change their content.

--- src/main/java/net/mcplayhd/lootrunstatistics/listeners/RenderListener.java
+++ src/main/java/net/mcplayhd/lootrunstatistics/listeners/RenderListener.java
@@ -1,5 +1,6 @@
 package net.mcplayhd.lootrunstatistics.listeners;
 
+import net.mcplayhd.lootrunstatistics.LootrunStatistics;
 import net.mcplayhd.lootrunstatistics.gui.CustomGui;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
@@ -9,6 +10,8 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import static net.mcplayhd.lootrunstatistics.LootrunStatistics.getChests;
 
 public class RenderListener {
+
+    private int lastPlayerLevel = 0;
 
     // idea to only open the gui one tick later from https://github.com/albarv340/chestcountmod
     @SubscribeEvent
@@ -21,6 +24,11 @@ public class RenderListener {
 
     @SubscribeEvent
     public void renderWorld(RenderWorldLastEvent event) {
+        int playerLevel = LootrunStatistics.getPlayerLevel();
+        if (lastPlayerLevel != playerLevel) {
+            lastPlayerLevel = playerLevel;
+            getChests().updateAllNotes();
+        }
         getChests().drawNotes();
     }
 
