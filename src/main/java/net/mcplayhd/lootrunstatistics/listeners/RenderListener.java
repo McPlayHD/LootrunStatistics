5fcc1159ea684ce6973e14c64796b012d97baad6
McPlayHD <McPlayHD@gmx.ch>
2022-06-14 17:16:28 +0200

    Adding a RenderTickEvent that will open CustomGUIs one tick delayed since you can't open them immediately when performing commands

--- src/main/java/net/mcplayhd/lootrunstatistics/listeners/RenderListener.java
+++ src/main/java/net/mcplayhd/lootrunstatistics/listeners/RenderListener.java
@@ -1,14 +1,27 @@
 package net.mcplayhd.lootrunstatistics.listeners;
 
-import net.mcplayhd.lootrunstatistics.LootrunStatistics;
+import net.mcplayhd.lootrunstatistics.gui.CustomGui;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import static net.mcplayhd.lootrunstatistics.LootrunStatistics.getChests;
 
 public class RenderListener {
 
+    // idea to only open the gui one tick later from https://github.com/albarv340/chestcountmod
     @SubscribeEvent
-    public void renderWorld(RenderWorldLastEvent e) {
-        LootrunStatistics.getChests().drawNotes();
+    public void onRenderTick(TickEvent.RenderTickEvent event) {
+        if (CustomGui.shouldBeDrawn != null) {
+            Minecraft.getMinecraft().displayGuiScreen(CustomGui.shouldBeDrawn);
+            CustomGui.shouldBeDrawn = null;
+        }
+    }
+
+    @SubscribeEvent
+    public void renderWorld(RenderWorldLastEvent event) {
+        getChests().drawNotes();
     }
 
 }
