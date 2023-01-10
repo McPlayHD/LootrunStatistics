e6b3ad078408b7a839c4b3301e51afa0acd9b46a
McPlayHD <McPlayHD@gmx.ch>
2023-01-10 14:46:28 +0100

    quickfix for distribution if you didn't find any items yet

--- src/main/java/net/mcplayhd/lootrunstatistics/commands/MainCommand.java
+++ src/main/java/net/mcplayhd/lootrunstatistics/commands/MainCommand.java
@@ -96,7 +96,7 @@ public class MainCommand extends CommandBase implements IClientCommand {
             sender.sendMessage(formatString("§3Distribution§7:"));
             for (Tier tier : Tier.values()) {
                 int amount = allItems.getOrDefault(tier, 0);
-                double percentage = amount / (double) total * 100;
+                double percentage = total == 0 ? 0 : amount / (double) total * 100;
                 DecimalFormat decimalFormat = new DecimalFormat("#0.0");
                 sender.sendMessage(formatString("§7  " + tier.getDisplayName() + "§7: §e" + getFormatted(amount) + " §7(§e" + decimalFormat.format(percentage) + "%§7)"));
             }
