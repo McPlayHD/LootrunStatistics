85454a98b477040e63858f61d2b5ca79ee348b93
McPlayHD <McPlayHD@gmx.ch>
2023-01-10 16:03:01 +0100

    bumping version to v1.1.0

--- src/main/java/net/mcplayhd/lootrunstatistics/LootrunStatistics.java
+++ src/main/java/net/mcplayhd/lootrunstatistics/LootrunStatistics.java
@@ -31,7 +31,8 @@ import java.util.UUID;
 public class LootrunStatistics {
     public static final String MODID = "lootrunstatistics";
     public static final String NAME = "Lootrun Statistics";
-    public static final String VERSION = "1.0.2";
+    // version needs to be updated here, in resources/version.txt and in build.gradle
+    public static final String VERSION = "1.1.0";
 
     private static Logger logger;
 
